05ea0c4a172cfbdd355c2abf6296e64135fd7d32
oskarziembrowicz <rakson2002@gmail.com>
2024-12-03 14:55:43 +0100

    Extracting JSX into component

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -69,8 +69,8 @@ function Header() {
 }
 
 function Menu() {
-  // const pizzas = pizzaData;
-  const pizzas = [];
+  const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
 
   return (
@@ -107,6 +107,8 @@ function Menu() {
 function Pizza(props) {
   console.log(props);
 
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -129,13 +131,12 @@ function Footer() {
   //   if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
   //   else alert("Sorry we're closed");
 
+  // if (!isOpen) return <p>CLOSED</p>;
+
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}. Come visit us or order online</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHours={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour} and {closeHour}
@@ -143,6 +144,17 @@ function Footer() {
       )}
     </footer>
   );
+
+  function Order(props) {
+    return (
+      <div className="order">
+        <p>
+          We're open until {props.closeHours}:00. Come visit us or order online
+        </p>
+        <button className="btn">Order</button>
+      </div>
+    );
+  }
 
   //   return React.createElement("footer", null, "We're currently open!");
 }
